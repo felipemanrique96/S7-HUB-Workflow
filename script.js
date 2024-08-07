d1620b483147d3c58a7a215707d62eaa8c1ca2c1
felipemanrique96 <felipe.manrique@settanta7.com>
2024-08-07 13:24:20 +0000

    update

--- script.js
+++ script.js
@@ -428,6 +428,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    
     // Wait for #projectTableBody to be available and then set up the MutationObserver
     waitForElement('#projectTableBody').then(targetNode => {
         const config = { childList: true, subtree: true };
