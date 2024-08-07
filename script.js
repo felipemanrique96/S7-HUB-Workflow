8e7f5feff42fa9af47f792e073816dc7ad5be6f2
felipemanrique96 <felipe.manrique@settanta7.com>
2024-08-07 13:34:51 +0000

    update

--- script.js
+++ script.js
@@ -423,6 +423,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+    
 
     // Wait for #projectTableBody to be available and then set up the MutationObserver
     waitForElement('#projectTableBody').then(targetNode => {
